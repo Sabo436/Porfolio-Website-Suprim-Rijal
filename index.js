6dea1de2e7dbce85df193c3af438557cd3c6a3d9
Suprim RIjal <suprimrijal1@gmail.com>
2025-02-06 21:41:11 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Change Navbar on Scroll
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
